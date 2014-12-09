cd48d7c1b0d58cd6f9c63104e35ef18cac0cd735
Emersion <contact@emersion.fr>
2014-12-09 22:12:39 +0100

    Adds play/pause/volume support and other improvements

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var exec = require('child_process').exec;
 var ClementineServer = require('clementine-remote').Server;
 var lame = require('lame');
 var Speaker = require('speaker');
+var Volume = require('pcm-volume');
 var taglib = require('taglib');
 
 var server = ClementineServer({
@@ -53,9 +54,41 @@ var scan = function (dir, done) {
 	});
 }
 
-var playing = false;
+var playing = false, fileStream, lameStream, streamFormat, volumeStream, pcmWriter;
+
+var position = 0, positionInterval;
+var startSendingPosition = function () {
+	positionInterval = setInterval(function () {
+		server.broadcast({
+			type: 'UPDATE_TRACK_POSITION',
+			response_update_track_position: { position: position }
+		});
+		position++;
+	}, 1000);
+};
+var stopSendingPosition = function () {
+	clearInterval(positionInterval);
+};
+
 server.on('playpause', function () {
-	if (playing) {
+	if (fileStream) {
+		if (playing) { // Pause
+			volumeStream.unpipe(pcmWriter);
+
+			stopSendingPosition();
+			server.broadcast({
+				type: 'PAUSE'
+			});
+		} else { // Play
+			volumeStream.pipe(pcmWriter);
+
+			startSendingPosition();
+			server.broadcast({
+				type: 'PLAY'
+			});
+		}
+		playing = !playing;
+		console.log('Playing:', playing);
 		return;
 	}
 	playing = true;
@@ -71,18 +104,36 @@ server.on('playpause', function () {
 
 			var index = Math.ceil(Math.random() * results.length);
 			var file = results[index];
-			taglib.tag(file, function (err, tag) {
+			taglib.read(file, function (err, tag, audioProperties) {
 				if (err) console.error('WARN: error while reading song tags', err);
 
-				console.log('Now playing:', file, tag);
+				console.log('Now playing:', file, tag, audioProperties);
 
-				// TODO: title from basename
+				// On new client:
+				//INFO
+				//CURRENT_METAINFO
+				//SET_VOLUME
+				//UPDATE_TRACK_POSITION
+				//PLAYLISTS
+				//PLAYLIST_SONGS
+				//REPEAT
+				//SHUFFLE
+				//FIRST_DATA_SENT_COMPLETE
+
+				// On play:
+				//ACTIVE_PLAYLIST_CHANGED
+				//CURRENT_METAINFO
+				//PLAY
+				//UPDATE_TRACK_POSITION
+				//STOP
+
 				var metadata = {
 					id: 0,
 					index: 0,
 					title: tag.title || path.basename(file),
 					filename: path.basename(file), //TODO: absolute path? Relative to music dir
-					is_local: true
+					is_local: true,
+					length: audioProperties.length
 				};
 				var props = ['album', 'artist', 'albumartist', 'track', 'disc', 'genre'];
 				for (var i = 0; i < props.length; i++) {
@@ -94,7 +145,7 @@ server.on('playpause', function () {
 				if (tag.year) {
 					metadata.pretty_year = String(tag.year);
 				}
-				//TODO: pretty_length, art, length, file_size, rating
+				//TODO: pretty_length, art, file_size, rating
 				server.broadcast({
 					type: 'CURRENT_METAINFO',
 					response_current_metadata: {
@@ -102,22 +153,40 @@ server.on('playpause', function () {
 					}
 				});
 
-				fs.createReadStream(file)
-					.pipe(new lame.Decoder())
-					.on('format', function (format) {
-						console.log('Started', format);
+				fileStream = fs.createReadStream(file);
+				lameStream = fileStream.pipe(new lame.Decoder());
+				lameStream.on('format', function (format) {
+						console.log('Started playing file:', format);
+						streamFormat = format;
 
 						server.broadcast({
 							type: 'ENGINE_STATE_CHANGED',
 							response_engine_state_changed: { state: 'Playing' }
 						});
+						server.broadcast({
+							type: 'PLAY'
+						});
 
-						this.pipe(new Speaker(format));
+						position = 0;
+						startSendingPosition();
+
+						pcmWriter = new Speaker(format);
+						volumeStream = new Volume();
+						volumeStream.pipe(pcmWriter);
+						this.pipe(volumeStream);
 					})
 					.on('end', function () {
 						console.log('Finished.');
 						playing = false;
+						fileStream = null;
+						lameStream = null;
+						volumeStream = null;
+						pcmWriter = null;
 
+						stopSendingPosition();
+						server.broadcast({
+							type: 'STOP'
+						});
 						server.broadcast({
 							type: 'ENGINE_STATE_CHANGED',
 							response_engine_state_changed: { state: 'Idle' }
@@ -126,4 +195,18 @@ server.on('playpause', function () {
 			});
 		});
 	});
+});
+
+server.on('volume', function (value) {
+	if (value < 0 || value > 100) {
+		return;
+	}
+	console.log('set volume', value / 100);
+	if (volumeStream) {
+		volumeStream.setVolume(value / 100);
+		server.broadcast({
+			type: 'SET_VOLUME',
+			request_set_volume: { volume: value }
+		});
+	}
 });
